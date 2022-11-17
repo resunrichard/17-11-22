779d451b1fb48cce733b5359914dd8f9e1347dbe
resunrichard <116341273+resunrichard@users.noreply.github.com>
2022-11-17 10:15:44 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,15 @@
+record Employee(int idno, String name, int salary) {
+Employee() {
+this(201,"Ramesh",25000); }
+public static void main(String args[]) {
+Student s=new Student(201,"Ramesh",25000);
+Student s1=new Student(102,"Surya",2500);
+Student s2=new Student(103,"Karthik",25000);
+Student s3=new Student(104,"Delhi",25000);
+Student s4=new Student(105,"Watson",25000);
+System.out.println(s);
+System.out.println(s1);
+System.out.println(s2);
+System.out.println(s3);
+System.out.println(s4);
+}}
---
+++ Vehicle.java
@@ -0,0 +1,15 @@
+record Vehicle(String vehicleno, String name, int price) {
+Vehicle() {
+this("AA1012","HONDA",150000); }
+public static void main(String args[]) {
+Student s=new Vehicle("AA1012","HONDA",150000);
+Student s1=new Vehicle("AA1022","MARUTHI",150000);
+Student s2=new Vehicle("AA1033","TVS",150000);
+Student s3=new Vehicle("AA1044","HIMALAYN",150000);
+Student s4=new Vehicle("AA1055","CITY+",150000);
+System.out.println(s);
+System.out.println(s1);
+System.out.println(s2);
+System.out.println(s3);
+System.out.println(s4);
+}}
